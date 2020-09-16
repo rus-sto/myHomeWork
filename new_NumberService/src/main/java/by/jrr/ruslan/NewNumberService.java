2300e7c4ddff0fdb5a55ad114fa296e80c3fad41
rus-sto <rus-sto@tut.by>
2020-09-16 23:14:49 +0300

    add best  sourse folder of new_NumberService to git

--- new_NumberService/src/main/java/by/jrr/ruslan/NewNumberService.java
+++ new_NumberService/src/main/java/by/jrr/ruslan/NewNumberService.java
@@ -1,19 +1,23 @@
 package by.jrr.ruslan;
 
 public class NewNumberService {
-    public int rangeSum (int start, int finish){
-        int sum  =0;
+    public int rangeSum(int start, int finish) {
+        int sum = 0;
         if (start < finish) {
-            for (int i =start; i <= finish; i++){
-                sum +=i;
+            for (int i = start; i <= finish; i++) {
+                sum += i;
             }
-        }else if (start> finish) {
-            for (int i = finish; i <=start; i++){
-                sum +=i; // the same as      sum =sum + i;
-            }
-        }else sum = 0;
+        }
+        return sum;
+    }
 
-        return  sum;
+    public int rangeEvenCount(int start, int finish) {
+        int sum = 0;
+        if (start > finish) {
+            for (int i = finish; i <= start; i++) {
+                sum += i; // the same as      sum =sum + i;
+            }
+        }return sum;
     }
 }
 
--- new_NumberService/src/test/java/by/jrr/ruslan/NewNumberServiceTest.java
+++ new_NumberService/src/test/java/by/jrr/ruslan/NewNumberServiceTest.java
@@ -7,24 +7,31 @@ import static org.junit.Assert.*;
 public class NewNumberServiceTest {
 
     @Test
-    public void rangeSumRightOrder() {
+    public void rangeSumTest() {
         NewNumberService numberService = new NewNumberService();
         int expected = 9;
         int actuale = numberService.rangeSum(2,4);
         assertEquals(expected,actuale);
     }
     @Test
-    public void rangeSumWrongOrder() {
+    public void rangeEvenCountTest() {
         NewNumberService numberService = new NewNumberService();
         int expected = 9;
-        int actuale = numberService.rangeSum(4,2);
+        int actuale = numberService.rangeEvenCount(4,2);
         assertEquals(expected,actuale);
     }
     @Test
-    public void rangeSumWithEqualNumbers() {
+    public void rangeSumWithEqualNumbersTest() {
         NewNumberService numberService = new NewNumberService();
         int expected = 0;
         int actuale = numberService.rangeSum(4,4);
+        assertEquals(expected,actuale);
+    }
+    @Test
+    public void rangeEvenCountWithEqualNumbersTest() {
+        NewNumberService numberService = new NewNumberService();
+        int expected = 0;
+        int actuale = numberService.rangeEvenCount(4,4);
         assertEquals(expected,actuale);
     }
 }
