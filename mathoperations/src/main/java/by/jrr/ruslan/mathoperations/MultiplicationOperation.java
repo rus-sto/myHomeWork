0bcddbfec750d86a45de53ffe6cc5b01426d7dcc
rus-sto <38364789+rus-sto@users.noreply.github.com>
2020-09-19 12:23:04 +0300

    Delete MultiplicationOperation.java
    delete math files

--- mathoperations/src/main/java/by/jrr/ruslan/mathoperations/MultiplicationOperation.java
+++
@@ -1,8 +0,0 @@
-package by.jrr.ruslan.mathoperations;
-
-public class MultiplicationOperation implements MathOperation{
-    @Override
-    public double compute (double a, double b){
-        return a * b;
-    }
-}
